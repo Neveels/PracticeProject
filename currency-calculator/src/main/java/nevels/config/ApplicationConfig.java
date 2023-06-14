ded7615ff2369101f70ca640a154267dd9232045
nevels <neevels@mail.ru>
2023-06-15 02:22:12 +0300

    change absolute path to relative

--- currency-calculator/src/main/java/nevels/config/ApplicationConfig.java
+++ currency-calculator/src/main/java/nevels/config/ApplicationConfig.java
@@ -1,19 +1,24 @@
 package nevels.config;
 
+import nevels.exception.BusinessException;
+
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class ApplicationConfig {
-    static final String FILE_NAME = ".\\src\\main\\resources\\application.properties";
+
+    static final String FILE_NAME = "\\src\\main\\resources\\application.properties";
     final String CURRENCY_DOLLAR = "currency.dollar";
 
     public double readFromFile() {
         Properties prop = new Properties();
-        try (FileInputStream fis = new FileInputStream(FILE_NAME)) {
+        String basePath = new File("").getAbsolutePath();
+        try (FileInputStream fis = new FileInputStream(basePath + FILE_NAME)) {
             prop.load(fis);
-        } catch (IOException ex) {
-            System.out.println(ex);
+        } catch (IOException e) {
+            throw new BusinessException(e.getMessage());
         }
         return Double.parseDouble(prop.getProperty(CURRENCY_DOLLAR));
     }
