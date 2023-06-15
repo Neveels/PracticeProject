ae22242a93cd9e916b54aa95eb5c6c91049111ba
nevels <neevels@mail.ru>
2023-06-15 12:50:49 +0300

    reformat code

--- currency-calculator/src/main/java/nevels/service/impl/CurrencyConversionImpl.java
+++ currency-calculator/src/main/java/nevels/service/impl/CurrencyConversionImpl.java
@@ -38,44 +38,44 @@ public class CurrencyConversionImpl implements CurrencyConversion {
 
         while (position < expText.length()) {
             char charater = expText.charAt(position);
-            if(charater == '(' || charater == '+' || charater == '-' || charater == ')') {
+            if (charater == '(' || charater == '+' || charater == '-' || charater == ')') {
                 lexemes.add(new Lexeme(LexemeType.getFromChar(String.valueOf(charater)), charater));
                 position++;
             } else {
-                    if(charater == 't') {
-                        String dollars = expText.substring(position, position + TO_DOLLARS_LENGTH);
-                        String rubles = expText.substring(position, position + TO_RUBLES_LENGTH);
-                        if (dollars.equals(LexemeType.TO_DOLLARS.getString())) {
-                            lexemes.add(new Lexeme(LexemeType.TO_DOLLARS, dollars));
-                            position += TO_DOLLARS_LENGTH;
-                        } else {
-                            lexemes.add(new Lexeme(LexemeType.TO_RUBLES, rubles));
-                            position += TO_RUBLES_LENGTH;
-                        }
-                        continue;
-                    }
-                    if (charater == '$') {
-                        position++;
-                    }
-                    charater = expText.charAt(position);
-                    if (charater <= '9' && charater >= '0') {
-                        StringBuilder stringBuilder = new StringBuilder();
-                        do {
-                            stringBuilder.append(charater);
-                            position++;
-                            if (position >= expText.length()) {
-                                break;
-                            }
-                            charater = expText.charAt(position);
-                        } while (charater <= '9' && charater >= '0' || charater == ',');
-                        lexemes.add(new Lexeme(LexemeType.NUMBER, stringBuilder.toString()));
+                if (charater == 't') {
+                    String dollars = expText.substring(position, position + TO_DOLLARS_LENGTH);
+                    String rubles = expText.substring(position, position + TO_RUBLES_LENGTH);
+                    if (dollars.equals(LexemeType.TO_DOLLARS.getString())) {
+                        lexemes.add(new Lexeme(LexemeType.TO_DOLLARS, dollars));
+                        position += TO_DOLLARS_LENGTH;
                     } else {
-                        if (charater != ' ' && charater != 'р' && charater != '$') {
-                            throw new BusinessException("Unexpected character: " + charater);
-                        }
-                        position++;// If we found white space, just skip it
+                        lexemes.add(new Lexeme(LexemeType.TO_RUBLES, rubles));
+                        position += TO_RUBLES_LENGTH;
                     }
+                    continue;
                 }
+                if (charater == '$') {
+                    position++;
+                }
+                charater = expText.charAt(position);
+                if (charater <= '9' && charater >= '0') {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    do {
+                        stringBuilder.append(charater);
+                        position++;
+                        if (position >= expText.length()) {
+                            break;
+                        }
+                        charater = expText.charAt(position);
+                    } while (charater <= '9' && charater >= '0' || charater == ',');
+                    lexemes.add(new Lexeme(LexemeType.NUMBER, stringBuilder.toString()));
+                } else {
+                    if (charater != ' ' && charater != 'р' && charater != '$') {
+                        throw new BusinessException("Unexpected character: " + charater);
+                    }
+                    position++;// If we found white space, just skip it
+                }
+            }
         }
         lexemes.add(new Lexeme(LexemeType.EOF, ""));
         return lexemes;
